29447dfcd67ab5e03178acf4e48779ff5b14651f
quadriphobs1 <abiodunquadriadekunle@gmail.com>
2019-11-07 11:41:20 +0100

    rust list implementation

---
+++ rust/list/README.md
@@ -0,0 +1,3 @@
+# Learn Rust With Entirely Too Many Linked Lists
+
+This my code implementation for tutorial on https://rust-unofficial.github.io/too-many-lists/index.html
---
+++ rust/list/src/lib.rs
@@ -0,0 +1,6 @@
+pub mod doubly_linked;
+pub mod doubly_singly_linked;
+pub mod persistent_singly_linked;
+pub mod simple_list;
+pub mod singly_linked;
+pub mod singly_linked_queue;
